3b27e986b2d43a4c637a9750c389ac5cc7eff2f9
Vinícius <boscardinvinicius@gmail.com>
2019-12-27 15:22:37 -0200

    Iniciado inclusão de novos estabelecimentos

--- packages/mobile/midow/lib/main.dart
+++ packages/mobile/midow/lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:background_fetch/background_fetch.dart';
 import 'package:flutter/material.dart';
 import 'package:midow/screen/home.dart';
 
-void main() => runApp(MyApp());
+void backgroundFetchHeadlessTask() async {
+  print('[BackgroundFetch] Headless event received.');
+  BackgroundFetch.finish();
+}
+
+void main() {
+  BackgroundFetch.registerHeadlessTask(backgroundFetchHeadlessTask);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- packages/mobile/midow/lib/screen/home.dart
+++ packages/mobile/midow/lib/screen/home.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:math';
 
+// import 'package:background_location/background_location.dart';
+import 'package:background_fetch/background_fetch.dart';
 import 'package:background_location/background_location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:math' as math;
 // import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,9 +14,15 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => HomePageState();
 }
 
-class HomePageState extends State<HomePage> {
+class HomePageState extends State<HomePage> with TickerProviderStateMixin {
   Completer<GoogleMapController> _controller = Completer();
   GlobalKey<ScaffoldState> _drawerKey = GlobalKey();
+  AnimationController _controllerAnimation;
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+  MarkerId selectedMarker;
+  int _markerIdCounter = 1;
+  LatLng markerAdd;
+
   int _selectedIndex = 1;
 
   List<Widget> menu = [
@@ -38,6 +48,12 @@ class HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    initPlatformState();
+
+    _controllerAnimation = new AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 500),
+    );
 
     BackgroundLocation.getPermissions(
       onGranted: () {
@@ -49,6 +65,7 @@ class HomePageState extends State<HomePage> {
     );
 
     BackgroundLocation.getLocationUpdates((location) async {
+      print('asdaaaaaaaaaaaaa');
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newCameraPosition(CameraPosition(
         target: LatLng(location.latitude, location.longitude),
@@ -57,112 +74,309 @@ class HomePageState extends State<HomePage> {
     });
   }
 
+  bool _enabled = true;
+  int _status = 0;
+  bool nova = false;
+  List<DateTime> _events = [];
+  List<IconData> icons = [Icons.info, Icons.add];
+
+  Future<void> initPlatformState() async {
+    BackgroundFetch.configure(
+        BackgroundFetchConfig(
+            minimumFetchInterval: 15,
+            stopOnTerminate: false,
+            startOnBoot: true,
+            enableHeadless: true,
+            requiresBatteryNotLow: false,
+            requiresCharging: false,
+            requiresStorageNotLow: false,
+            requiresDeviceIdle: false,
+            requiredNetworkType: BackgroundFetchConfig.NETWORK_TYPE_NONE),
+        () async {
+      // This is the fetch-event callback.
+      print('[BackgroundFetch] Event received');
+      setState(() {
+        _events.insert(0, new DateTime.now());
+      });
+      // IMPORTANT:  You must signal completion of your fetch task or the OS can punish your app
+      // for taking too long in the background.
+      BackgroundFetch.finish();
+    }).then((int status) {
+      print('[BackgroundFetch] configure success: $status');
+      setState(() {
+        _status = status;
+      });
+    }).catchError((e) {
+      print('[BackgroundFetch] configure ERROR: $e');
+      setState(() {
+        _status = e;
+      });
+    });
+
+    int status = await BackgroundFetch.status;
+    setState(() {
+      _status = status;
+    });
+    if (!mounted) return;
+  }
+
+  void _onClickEnable(enabled) {
+    setState(() {
+      _enabled = enabled;
+    });
+    if (enabled) {
+      BackgroundFetch.start().then((int status) {
+        print('[BackgroundFetch] start success: $status');
+      }).catchError((e) {
+        print('[BackgroundFetch] start FAILURE: $e');
+      });
+    } else {
+      BackgroundFetch.stop().then((int status) {
+        print('[BackgroundFetch] stop success: $status');
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Color backgroundColor = Theme.of(context).cardColor;
     return new Scaffold(
-        key: _drawerKey,
-        drawer: Drawer(
-            // Add a ListView to the drawer. This ensures the user can scroll
-            // through the options in the drawer if there isn't enough vertical
-            // space to fit everything.
+      key: _drawerKey,
+      drawer: Drawer(
+          // Add a ListView to the drawer. This ensures the user can scroll
+          // through the options in the drawer if there isn't enough vertical
+          // space to fit everything.
 
-            child: Container(
-          color: Colors.white,
-          child: ListView.builder(
-              itemCount: menu.length + 1,
-              itemBuilder: (context, index) {
-                if (index == 0) {
-                  return SizedBox(
-                      height: 160,
-                      child: DrawerHeader(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: <Widget>[
-                            FloatingActionButton(
-                                disabledElevation: 0,
-                                onPressed: null,
-                                child: Icon(Icons.person),
-                                backgroundColor:
-                                    Theme.of(context).primaryColor),
-                            Text('Não cadastrado'),
-                            Text('Clique para cadastra-se')
-                          ],
-                        ),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                        ),
-                      ));
-                } else {
-                  return Container(
-                      padding: EdgeInsets.all(10.0),
-                      child: Material(
-                          elevation:
-                              _selectedIndex != null && _selectedIndex == index
-                                  ? 2.0
-                                  : 0,
-                          borderRadius: BorderRadius.circular(5.0),
-                          color:
-                              _selectedIndex != null && _selectedIndex == index
-                                  ? Theme.of(context).primaryColor
-                                  : Colors.transparent,
-                          child: ListTile(
-                            title: menu[index - 1],
-                            onTap: () {
-                              _selectedIndex = index;
-                              setState(() {});
-                            },
-                          )));
+          child: Container(
+        color: Colors.white,
+        child: ListView.builder(
+            itemCount: menu.length + 1,
+            itemBuilder: (context, index) {
+              if (index == 0) {
+                return GestureDetector(
+                    onDoubleTap: () {
+                      _onClickEnable(true);
+                    },
+                    child: SizedBox(
+                        height: 160,
+                        child: DrawerHeader(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: <Widget>[
+                              FloatingActionButton(
+                                  disabledElevation: 0,
+                                  onPressed: null,
+                                  child: Icon(Icons.person),
+                                  backgroundColor:
+                                      Theme.of(context).primaryColor),
+                              Text('Não cadastrado'),
+                              Text('Clique para cadastrar-se')
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                          ),
+                        )));
+              } else {
+                return Container(
+                    padding: EdgeInsets.all(10.0),
+                    child: Material(
+                        elevation:
+                            _selectedIndex != null && _selectedIndex == index
+                                ? 2.0
+                                : 0,
+                        borderRadius: BorderRadius.circular(5.0),
+                        color: _selectedIndex != null && _selectedIndex == index
+                            ? Theme.of(context).primaryColor
+                            : Colors.transparent,
+                        child: ListTile(
+                          title: menu[index - 1],
+                          onTap: () {
+                            _selectedIndex = index;
+                            setState(() {});
+                          },
+                        )));
+              }
+            }),
+      )),
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            // child:
+            child: GoogleMap(
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              markers: Set<Marker>.of(markers.values),
+              onCameraIdle: () {
+                print('aqui');
+              },
+              onCameraMove: (object) {
+                if (nova) {
+                  _remove();
+                  markerAdd = object.target;
+                  final String markerIdVal = 'marker_id_$_markerIdCounter';
+                  final MarkerId markerId = MarkerId(markerIdVal);
+
+                  final Marker marker = Marker(
+                      markerId: markerId,
+                      position: LatLng(markerAdd.latitude, markerAdd.longitude),
+                      infoWindow: InfoWindow(title: markerIdVal, snippet: '*'),
+                      onTap: () {
+                        // _onMarkerTapped(markerId);
+                      });
+                  markers[markerId] = marker;
+                  setState(() {});
                 }
-              }),
-        )),
-        body: Stack(
-          children: <Widget>[
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              bottom: 0,
-              // child:
-              child: GoogleMap(
-                myLocationEnabled: true,
-                myLocationButtonEnabled: false,
-                mapType: MapType.normal,
-                initialCameraPosition: _kGooglePlex,
-                onMapCreated: (GoogleMapController controller) {
-                  _controller.complete(controller);
+              },
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              height: 120,
+              child: AppBar(
+                leading: Transform.scale(
+                    scale: 0.7,
+                    child: FloatingActionButton(
+                      backgroundColor: Theme.of(context).primaryColor,
+                      child: Icon(Icons.menu),
+                      onPressed: () {
+                        _drawerKey.currentState.openDrawer();
+                      },
+                    )),
+                backgroundColor: Colors.transparent,
+                elevation: 0,
+                title: Text('Midow'),
+                bottom: PreferredSize(
+                  child: Container(
+                    color: Colors.orange,
+                    height: 0,
+                  ),
+                  preferredSize: Size.fromHeight(0.0),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: !this.nova
+          ? new Column(
+              mainAxisSize: MainAxisSize.min,
+              children: new List.generate(icons.length, (int index) {
+                Widget child = new Container(
+                  height: 70.0,
+                  width: 56.0,
+                  alignment: FractionalOffset.topCenter,
+                  child: new ScaleTransition(
+                    scale: new CurvedAnimation(
+                      parent: _controllerAnimation,
+                      curve: new Interval(0.0, 1.0 - index / icons.length / 2.0,
+                          curve: Curves.easeOut),
+                    ),
+                    child: new FloatingActionButton(
+                      elevation: 8,
+                      heroTag: null,
+                      backgroundColor: backgroundColor,
+                      mini: true,
+                      child: new Icon(
+                        icons[index],
+                        color: Theme.of(context).primaryColor,
+                        size: 15,
+                      ),
+                      onPressed: () async {
+                        switch (index) {
+                          case 1:
+                            this.nova = true;
+                            _controllerAnimation.reverse();
+                            final String markerIdVal =
+                                'marker_id_$_markerIdCounter';
+                            final MarkerId markerId = MarkerId(markerIdVal);
+
+                            final Marker marker = Marker(
+                                markerId: markerId,
+                                position: LatLng(
+                                    markerAdd.latitude, markerAdd.longitude),
+                                infoWindow: InfoWindow(
+                                    title: markerIdVal, snippet: '*'),
+                                onTap: () {
+                                  // _onMarkerTapped(markerId);
+                                });
+
+                            setState(() {
+                              markers[markerId] = marker;
+                            });
+
+                            break;
+                          default:
+                        }
+                      },
+                    ),
+                  ),
+                );
+                return child;
+              }).toList()
+                ..add(
+                  new FloatingActionButton(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    heroTag: null,
+                    child: new AnimatedBuilder(
+                      animation: _controllerAnimation,
+                      builder: (BuildContext context, Widget child) {
+                        return new Transform(
+                          transform: new Matrix4.rotationZ(
+                              _controllerAnimation.value * 0.5 * math.pi),
+                          alignment: FractionalOffset.center,
+                          child: new Icon(_controllerAnimation.isDismissed
+                              ? Icons.share
+                              : Icons.close),
+                        );
+                      },
+                    ),
+                    onPressed: () {
+                      if (_controllerAnimation.isDismissed) {
+                        _controllerAnimation.forward();
+                      } else {
+                        _controllerAnimation.reverse();
+                      }
+                    },
+                  ),
+                ),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width * 0.5,
+              child: RaisedButton(
+                color: Theme.of(context).primaryColor,
+                textColor: Colors.white,
+                child: Text('Cancelar'),
+                onPressed: () {
+                  this.nova = !this.nova;
+                  this.markers = <MarkerId, Marker>{};
+                  setState(() {});
                 },
               ),
             ),
-            Positioned(
-              top: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                height: 120,
-                child: AppBar(
-                  leading: Transform.scale(
-                      scale: 0.7,
-                      child: FloatingActionButton(
-                        backgroundColor: Theme.of(context).primaryColor,
-                        child: Icon(Icons.menu),
-                        onPressed: () {
-                          _drawerKey.currentState.openDrawer();
-                        },
-                      )),
-                  backgroundColor: Colors.transparent,
-                  elevation: 0,
-                  title: Text('Midow'),
-                  bottom: PreferredSize(
-                    child: Container(
-                      color: Colors.orange,
-                      height: 0,
-                    ),
-                    preferredSize: Size.fromHeight(0.0),
-                  ),
-                ),
-              ),
-            )
-          ],
-        ));
+      floatingActionButtonLocation: this.nova
+          ? FloatingActionButtonLocation.centerFloat
+          : FloatingActionButtonLocation.endFloat,
+    );
+  }
+
+  void _remove() {
+    setState(() {
+      if (markers.containsKey(selectedMarker)) {
+        markers.remove(selectedMarker);
+      }
+    });
   }
 }
